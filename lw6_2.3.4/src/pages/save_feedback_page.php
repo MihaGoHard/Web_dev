e1462ca0b6a9d0e644f782322c08922d2630c6b8
MihaGoHard <shasherin06@bk.ru>
2020-05-03 12:45:04 +0300

    lw 5.2

--- lw6_2.3.4/src/pages/save_feedback_page.php
+++ lw6_2.3.4/src/pages/save_feedback_page.php
@@ -1,27 +1,49 @@
 <?
 function updateUserData() 
-{
-    $email = getPOSTParameter('email');
-    if ($email)                                         
-    {                          
-        $keys = ['First Name', 'Email', 'Country', 'Messege', 'Gender']; // параметры
-        $data = [];
+{                  
+    $keys = ['First Name', 'Email', 'Country', 'Messege', 'Gender']; // параметры
+    $data = [];
+    $status = ['email' => 'mistace', 'name' => 'mistace', 'messege' => 'mistace'];
 
-        foreach ($keys as $key)
-        {
-            $formatedKey = str_replace(' ', '_', strtolower($key));
-            $data[$key . ':'] = getPOSTParameter($formatedKey);
-        }
+    foreach ($keys as $key)
+    {
+        $formatedKey = str_replace(' ', '_', strtolower($key));
+        $data[$key . ':'] = getPOSTParameter($formatedKey);
+    }
+    
+    if ((filter_var($data['Email:'], FILTER_VALIDATE_EMAIL) == '') || ($data['Email:'] === ''))
+    {
+        $status['email'] = 'mistace'; 
+    }
+    else
+    {
+        $status['email'] = 'ok';
+        $email = $email = getPOSTParameter('email');
+    }     
 
+    if ((($data['First Name:'] != '') && (!preg_match_all('/^[a-zA-Z]+$/', $data['First Name:'])) || ($data['First Name:'] === '')))
+    {
+        $status['First Name:'] = 'mistace';
+    }     
+    else
+    {
+        $status['name'] = 'ok';
+    }
+
+    ($data['Messege:'] === '') ? $status['messege'] = 'mistace' : $status['messege'] = 'ok';
+    
+    if (($status['email'] === 'ok') && ($status['name'] === 'ok') && ($status['messege'] === 'ok'))
+    { 
         $toFileData = $data;
         $fileAddres = "../src/user_data/" . strtolower($email) . ".txt";   
-            
+        
         if (file_exists($fileAddres))                                      // проверка существования такого файла
         {
             $fileUserData = getDataFromFile($fileAddres);
             $toFileData = joinArrays($fileUserData, $data);                // обновление файла данными из query string       
         }
         addDataToFile($toFileData, $fileAddres);
-    }   
-    renderTemplate('form.tpl.php', []);
+        $status = 'Отправлено';
+    }  
+    renderTemplate('form.tpl.php', [$status, $data]);
 }
--- lw6_2.3.4/src/templates/feedback.tpl.php
+++ lw6_2.3.4/src/templates/feedback.tpl.php
@@ -1,41 +1,65 @@
-<form method="POST">
-  <div class="zapros">
-    <div class="form-window form-window_email">
-      <span class="form-name">
-        email пользователя
-        <span class="form-name form-name_marker">*</span>
-      </span> 
-      <input class="form-feel" type="text" name="email" value="<? ($args['Email: '] != null) ? print $args['Email: '] : print_r($args[1]); ?>" autocomplete="off" /> <br />
+<!DOCTYPE html>
+<html lang="ru">
+  <meta http-equiv="Cache-Control" content="no-cache">
+  <head>
+    <meta charset="UTF-8">
+    <title>AnDzanDzan</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=swap&subset=cyrillic" rel="stylesheet">
+    <link rel="stylesheet" href="css/buttons_fb.css">
+    <link rel="stylesheet" href="css/main_fb.css">
+    <link rel="stylesheet" href="css/forms-block_fb.css">
+  </head>
+  <body style="margin: 0">
+    <div class="main"> 
+
+      <div class="forms-block">
+        <div class="forms-head">
+          <img alt="netu"src="img/line-left.png" class="forms-img">
+          <h1 class="forms-head-text">ПОИСК АНКЕТЫ</h1>
+          <img alt="netu"src="img/line-right.png" class="forms-img">
+        </div>
+
+        <form method="POST">
+          <div class="zapros">
+            <div class="form-window form-window_email">
+              <span class="form-name">
+                email пользователя
+                <span class="form-name form-name_marker">*</span>
+              </span> 
+              <input class="form-feel" type="text" name="email" value="<? ($args['Email: '] != null) ? print $args['Email: '] : print_r($args[1]); ?>" autocomplete="off" /> <br />
+            </div>
+            <div>
+              <button class="button button_email" type="submit">
+                Найти анкету
+              </button> 
+              <a class="button_search" href="../web/index.php">Вернуться на главную</a>
+            </div> 
+          </div>   
+        </form>
+
+        <? if ($args['Email: '] != null): ?>
+          <div class="user-data">
+            <span class="form-name">Данные пользователя</span>
+            <ul class="list">
+              <? foreach($args as $key => $value): ?>
+                <li class="list-element">
+                  <? echo $key . $value; ?>
+                </li>
+              <? endforeach; ?>
+            </ul> 
+          </div>
+        <? endif; ?>
+
+        <? if ($args['Email: '] === null): ?>
+          <span class="form-name form-name_mistace">
+            <? ($args[0] === null) ? $args[0] = 'Введите Email пользователя' : null; ?>
+            <? print_r($args[0]); ?>
+            <span class="form-name form-name_marker">*</span>
+          </span>
+        <? endif; ?>
     </div>
-    <div>
-      <button class="button button_email" type="submit">
-        Найти анкету
-      </button> 
-      <a class="button_search" href="../web/index.php">Вернуться на главную</a>
-    </div> 
-  </div>   
-</form>
-
-<? if ($args['Email: '] != null): ?>
-  <div class="user-data">
-    <span class="form-name">Данные пользователя</span>
-    <ul class="list">
-      <? foreach($args as $key => $value): ?>
-        <li class="list-element">
-          <? echo $key . $value; ?>
-        </li>
-      <? endforeach; ?>
-    </ul> 
-  </div>
-<? endif; ?>
-
-<? if ($args['Email: '] === null): ?>
-  <span class="form-name form-name_mistace">
-    <? ($args[0] === null) ? $args[0] = 'Введите Email пользователя' : null; ?>
-    <? print_r($args[0]); ?>
-    <span class="form-name form-name_marker">*</span>
-  </span>
-<? endif; ?>
+  </body>    
+</html>
 
 
 
