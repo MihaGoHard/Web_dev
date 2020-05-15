42bfdf1ddc14169fe50937efa045b63580d74cc3
MihaGoHard <shasherin06@bk.ru>
2020-05-15 13:09:01 +0300

    lw9

--- lw9/js/fetch.js
+++ lw9/js/fetch.js
@@ -1,30 +1,21 @@
-function lightFeel(validResult) {
-  if (validResult['status'] != 'succes') {
-    const status = '.default';
-    for (let key in validResult) {
-      if (key != 'status') {
-        document.getElementById(`${key}`).style=`border-color:${validResult[key]}`;      
-      } 
-    }
-    showStatus(status);
-  }
-  if (validResult['status'] == 'succes') {
-    const status = '.succes';
-    for (let key in validResult) {
-      if (key != 'status' && key != 'country' && key != 'gender') {
-        document.getElementById(`${key}`).style=`border-color:""`; 
-        document.getElementById(`${key}`).value="";    
-      }
-    }
-    showStatus(status);
-  }
+function start() {
+  runCicle();
+  const submitEvent = document.querySelector('form');
+  submitEvent.addEventListener('submit', () => sentRequest(event));
 }
+window.onload = start;
 
-function showStatus(status) {
-  document.querySelector(status).style='opacity:100%'; 
-  setTimeout(() => {
-    document.querySelector(status).style='opacity:0';
-  }, 2000)   
+async function sentRequest(event) {
+  event.preventDefault();
+  if (event.defaultPrevented) {
+    const validResult = await sendRequest();
+    console.log(validResult);
+    if (validResult != null) {
+      lightFeel(validResult)
+    } else {
+      console.log('Something went wrong');
+    }   
+  }
 }
 
 function sendRequest() { 
@@ -43,23 +34,50 @@ function sendRequest() {
   }) 
 }
 
-async function sentRequest() {
-  event.preventDefault();
-  if (event.defaultPrevented) {
-    const validResult = await sendRequest();
-    console.log(validResult);
-    if (validResult != null) {
-      lightFeel(validResult)
-    } else {
-      console.log('Something went wrong');
-    }   
+function lightFeel(validResult) {
+  if (validResult['status'] != 'succes') {
+    chooseBorderColor(validResult);
+    showStatus('disappear');
+  }
+  if (validResult['status'] == 'succes') {
+    resetAllParameter(validResult);
+    showStatus('appear');
   }
 }
 
-function start() {
-  runCicle();
-  const submitEvent = document.querySelector('form');
-  submitEvent.addEventListener('submit', () => sentRequest());
+function chooseBorderColor(validResult) {
+  for (let key in validResult) {
+    (key != 'status' && validResult[key] == 'red') 
+      ? changeClassName(key, 'form_feel', 'form_feel_red') : null;        
+    (key != 'status' && key != 'gender' && key != 'country' && validResult[key] == '') 
+      ? changeClassName(key, 'form_feel_red', 'form_feel') : null;  
+  }  
 }
 
-window.onload = start; 
+function resetAllParameter(validResult) {
+  for (let key in validResult) {
+    if (key != 'status' && key != 'country' && key != 'gender') {
+      changeClassName(key, 'form_feel_red', 'form_feel'); 
+      document.getElementById(`${key}`).value="";    
+    }
+  }
+}
+
+function changeClassName(key, removedClass, addedClass) {
+  let classList = document.getElementById(`${key}`).classList;
+  if (classList.contains(removedClass)) {  
+    classList.remove(removedClass); 
+    classList.add(addedClass);
+  }
+}
+
+function showStatus(effect) {
+  classList = document.getElementById('sent_messege').classList;
+  (classList.contains('appearence') && effect == 'disappear' ) ? classList.remove('appearence') : null;
+  (!classList.contains('appearence') && effect == 'appear' ) ? classList.add('appearence') : null;      
+}
+
+
+
+
+ 
