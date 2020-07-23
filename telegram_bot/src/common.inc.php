a597288f1a232f0e3261be17a75ccb0291cdbb67
MihaGoHard <shasherin06@bk.ru>
2020-07-23 13:05:08 +0000

    B

--- telegram_bot/src/common.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('config/config.php');
-require_once('utils/request.php');
-require_once('utils/database.php');
-require_once('utils/response.php');
-require_once('utils/buttons_answer.php');
-require_once('utils/api_works.php');
-
-
