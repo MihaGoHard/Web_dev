86dde9a538d8e30182885615bad7cec2b1146758
MihaGoHard <shasherin06@bk.ru>
2020-07-23 13:16:04 +0000

    telegram__bot

---
+++ telegramBot/README.md
@@ -0,0 +1,20 @@
+# telegramBot
+## src  
+  * config  
+      -config.php  
+  * shema  
+      -database.sql  
+  * utils  
+       -api_works.php
+       -buttons_answer.php	
+       -database.php  
+       -request.php  
+       -tesponse.php  
+  * ### common.inc.php  
+##   
+* composer.json  
+* index.php  
+* sent.php    
+## README.DM    
+
+     
---
+++ telegramBot/src/shema/database.sql
@@ -0,0 +1,17 @@
+USE heroku_dddd;
+CREATE TABLE chat_info
+(
+    main_id               INT AUTO_INCREMENT  NOT NULL,
+    chat_id               INT                 NOT NULL,
+    start_time            INT                 NOT NULL,
+    subscribe_button      INT                 NOT NULL,
+    describe_button       INT                 NOT NULL,
+    time_button           INT                 NOT NULL,
+    update_button         INT                 NOT NULL,
+    PRIMARY KEY (main_id)
+) DEFAULT CHARACTER SET utf8mb4
+  COLLATE `utf8mb4_unicode_ci`
+  ENGINE = InnoDB
+;
+
+
